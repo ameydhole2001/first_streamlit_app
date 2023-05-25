13f6a15c032aa6fd5ba186752f070548a6e26732
ameydhole2001 <122972332+ameydhole2001@users.noreply.github.com>
2023-05-25 15:48:35 +0530

    Update streamlit_app.py
    Have set the index

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ streamlit.text('Fruits List..!!')
 
 # Reading From S3 Bucket
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-
+my_fruit_list = my_fruit_list.set_index('Fruit')
 #streamlit To Display whats there in the Data Frame
 streamlit.dataframe(my_fruit_list)
 
