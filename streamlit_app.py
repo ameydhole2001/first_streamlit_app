c7ababfcbdcbe5f4c5575cacf0c39c15b397db8d
ameydhole2001 <122972332+ameydhole2001@users.noreply.github.com>
2023-05-25 17:07:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,17 +13,17 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 streamlit.dataframe(my_fruit_list)
 
 # Let's put a pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+# streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 # Display the table on the page.
 streamlit.dataframe(my_fruit_list)
 
 
 # #fILTER
-# streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+# streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 #STORE THE LIST OF SELECTED FRUITS INTO A SELECTED VARIABLE 
-fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 #USE to Access a group of rows and columns by label(s) or a boolean array.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
