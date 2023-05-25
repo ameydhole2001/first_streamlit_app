726bf022da2e12520c5552f403e745283040966a
ameydhole2001 <122972332+ameydhole2001@users.noreply.github.com>
2023-05-25 14:54:49 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,13 @@ import streamlit
 streamlit.title("Hey Getting Started")
 
 streamlit.header('DATA APP BUILDERS')
-streamlit.text('Setting Up a connection between Streamlit and Git')
+
+streamlit.text('Fruits List..!!')
+
+# Reading From S3 Bucket
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+#streamlit To Display whats there in the Data Frame
+streamlit.dataframe(my_fruit_list)
+
+
