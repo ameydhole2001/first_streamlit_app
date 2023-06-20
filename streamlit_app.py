7a6dbe03fdef5dc8bdda08be56950899c4e7c900
ameydhole2001 <122972332+ameydhole2001@users.noreply.github.com>
2023-06-20 17:00:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,34 +40,71 @@
 # streamlit.dataframe(fruityvice_normalized)
 # import snowflake.connector
 
-import streamlit
-import pandas
+# import streamlit
+# import pandas
+# import requests
+# import snowflake.connector
+# from urllib.error import URLError
+# streamlit.title('My Mom\'s New Healthy Dinner')
+# streamlit.header('Breakfast Favourites')
+# streamlit.text(':bowl_with_spoon: Ómega 3 & Blueberry Oatmeal')
+# streamlit.text(':green_salad: Kale, Spinach & Rocket Smmoothie')
+# streamlit.text(':chicken:Hard-Boiled Free-Range Egg')
+# streamlit.text(':avocado::bread: Avocado Toast')
+# streamlit.header(':banana::mango: Build Your Own Fruit Smoothie :kiwifruit::grapes:')
+# #import pandas
+# my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+# my_fruit_list = my_fruit_list.set_index('Fruit')
+# fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
+# fruits_to_show = my_fruit_list.loc[fruits_selected]
+# streamlit.dataframe(fruits_to_show)
+# def get_fruityvice_data(this_fruit_choice):
+#   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+#   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#   return fruityvice_normalized
+# streamlit.header("Fruityvice Fruit Advice!")
+# try:
+#   fruit_choice = streamlit.text_input('What fruit would you like information about?')
+#   if not fruit_choice:
+#     streamlit.error("Please select a fruit to get information.")
+#   else:
+#     back_from_function = get_fruityvice_data(fruit_choice)
+#     streamlit.dataframe(back_from_function)
+# streamlit.stop()
+import streamlit as st
+import pandas as pd
 import requests
-import snowflake.connector
-from urllib.error import URLError
-streamlit.title('My Mom\'s New Healthy Dinner')
-streamlit.header('Breakfast Favourites')
-streamlit.text(':bowl_with_spoon: Ómega 3 & Blueberry Oatmeal')
-streamlit.text(':green_salad: Kale, Spinach & Rocket Smmoothie')
-streamlit.text(':chicken:Hard-Boiled Free-Range Egg')
-streamlit.text(':avocado::bread: Avocado Toast')
-streamlit.header(':banana::mango: Build Your Own Fruit Smoothie :kiwifruit::grapes:')
-#import pandas
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('Fruit')
-fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
+
+st.title("My Mom's New Healthy Dinner")
+st.header("Breakfast Favourites")
+st.text(":bowl_with_spoon: Ómega 3 & Blueberry Oatmeal")
+st.text(":green_salad: Kale, Spinach & Rocket Smoothie")
+st.text(":chicken: Hard-Boiled Free-Range Egg")
+st.text(":avocado::bread: Avocado Toast")
+
+# Load fruit list
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index("Fruit")
+
+# Fruit selection
+fruits_selected = st.multiselect("Pick some fruits:", list(my_fruit_list.index), ["Avocado", "Strawberries"])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
-streamlit.dataframe(fruits_to_show)
+st.dataframe(fruits_to_show)
+
+# Function to get Fruityvice data
 def get_fruityvice_data(this_fruit_choice):
-  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
-  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-  return fruityvice_normalized
-streamlit.header("Fruityvice Fruit Advice!")
-try:
-  fruit_choice = streamlit.text_input('What fruit would you like information about?')
-  if not fruit_choice:
-    streamlit.error("Please select a fruit to get information.")
-  else:
-    back_from_function = get_fruityvice_data(fruit_choice)
-    streamlit.dataframe(back_from_function)
-streamlit.stop()
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
+
+st.header("Fruityvice Fruit Advice!")
+fruit_choice = st.text_input("What fruit would you like information about?")
+if not fruit_choice:
+    st.error("Please select a fruit to get information.")
+else:
+    try:
+        back_from_function = get_fruityvice_data(fruit_choice)
+        st.dataframe(back_from_function)
+    except requests.exceptions.RequestException as e:
+        st.error("An error occurred while fetching the data. Please try again later.")
+
